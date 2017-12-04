f39c909cfc3fc6552f4760ad928d10d52ca02704
nguyenphuckhanh1991 <khanhnguyen.joy@gmail.com>
2017-12-04 17:49:29 +0700

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult .swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult .swift
+//  StoreSearch
+//
+//  Created by Joy on 12/4/17.
+//  Copyright © 2017 nguyen.phuc.khanh. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+  var name = ""
+  var artistName = ""
+}
