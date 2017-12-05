960e382ed5bc771eb815d6bbe54840b3d7c3f780
nguyenphuckhanh1991 <khanhnguyen.joy@gmail.com>
2017-12-05 11:37:27 +0700

    URLSessionDownloadTask

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -169,21 +169,6 @@ class SearchViewController: UIViewController {
     }
     return searchResult
   }
-  func kindForDisplay(_ kind: String) -> String {
-    switch kind {
-    case "album": return "Album"
-    case "audiobook": return "Audio Book"
-    case "book": return "Book"
-    case "ebook": return "E-Book"
-    case "feature-movie": return "Movie"
-    case "music-video": return "Music Video"
-    case "podcast": return "Podcast"
-    case "software": return "App"
-    case "song": return "Song"
-    case "tv-episode": return "TV Episode"
-    default: return kind
-    }
-  }
   func showNetworkError() {
     let alert = UIAlertController(title: "Whoops...", message: "There was an error reading from the iTunes Store. Please try again.", preferredStyle: .alert)
     let action = UIAlertAction(title: "OK", style: .default, handler: nil)
@@ -259,12 +244,7 @@ extension SearchViewController: UITableViewDataSource {
     } else {
       let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCellIdentifiers.searchResultCell, for: indexPath) as! SearchResultCell
       let searchResult = searchResults[indexPath.row]
-      cell.nameLabel.text = searchResult.name
-      if searchResult.artistName.isEmpty {
-        cell.artistNameLabel.text = "Unknown"
-      } else {
-        cell.artistNameLabel.text = String(format: "%@ (%@)", searchResult.artistName, kindForDisplay(searchResult.kind))
-      }
+      cell.configure(for: searchResult)
       return cell
     }
   }
